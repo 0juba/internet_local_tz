cb5788cdb478ccdbfb91a9da6f3c0ab5cd9e8d01
Guba Andrei <xg.andrew@gmail.com>
2016-11-22 03:31:38 +1000

    added strategies for different kind of figures

---
+++ src/Domain/FigureStrategy/KingStrategy.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ChessDomain\FigureStrategy;
+
+class KingStrategy implements FigureStrategyInterface 
+{
+    public function moveTo()
+    {
+        // TODO: Implement moveTo() method.
+    }
+}
---
+++ src/Domain/FigureStrategy/QueenStrategy.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ChessDomain\FigureStrategy;
+
+class QueenStrategy implements FigureStrategyInterface 
+{
+    public function moveTo()
+    {
+        // TODO: Implement moveTo() method.
+    }
+}
