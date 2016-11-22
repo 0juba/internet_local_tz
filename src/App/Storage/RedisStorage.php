11cc653d72a2f264f5318e15cfdb0e01d97578a0
Guba Andrei <xg.andrew@gmail.com>
2016-11-22 12:39:51 +1000

    impl redis storage

--- src/App/Storage/RedisStorage.php
+++ src/App/Storage/RedisStorage.php
@@ -7,13 +7,26 @@ use ChessDomain\Storage\StorageInterface;
 
 class RedisStorage implements StorageInterface 
 {
+    const BOARD_KEY = 'chess:board';
+
+    private $redisClient;
+
+    public function __construct($host, $port = 6379)
+    {
+        $this->redisClient = new \Predis\Client(array(
+            'scheme' => 'tcp',
+            'host'   => $host,
+            'port'   => $port,
+        ));
+    }
+
     public function save(ChessBoard $chessBoard)
     {
-        
+       $this->redisClient->set(self::BOARD_KEY, serialize($chessBoard));
     }
 
     public function load()
     {
-        
+        return unserialize($this->redisClient->get(self::BOARD_KEY));
     }
 }
