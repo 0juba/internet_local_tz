70d9603fde6ac8bc409e33222d07e60327210813
Guba Andrei <xg.andrew@gmail.com>
2016-11-22 11:36:49 +1000

    added skeleton for listeners

---
+++ src/App/Listener/AnyFigureListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ChessApp\Listener;
+
+use ChessDomain\Notifier\Event;
+use ChessDomain\Notifier\ListenerInterface;
+
+class AnyFigureListener implements ListenerInterface
+{
+    public function handle(Event $event)
+    {
+        // TODO impl some code
+    }
+}
